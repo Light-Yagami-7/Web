f67571e047e5d1f03758fa8134ffd7f5c1427233
Light-Yagami-7 <charlieken221@gmail.com>
2024-09-25 22:31:54 +0530

    Files have been changed in the last 24 hours

--- beta.sh
+++ beta.sh
@@ -3,12 +3,10 @@
 modified=$(find -type f -mtime -1)
 
 if [[ -n $modified ]]; then
-for file in $modified; do
-	git add $file
+
+	git add .
 	git commit -m "Files have been changed in the last 24 hours"
 	git push
-
-done
 
 else 
 	echo "No changes have been found"
