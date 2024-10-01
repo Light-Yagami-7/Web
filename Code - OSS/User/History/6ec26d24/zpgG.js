087c37efb496d2622bca32ddc0eccff71398ccc5
Light-Yagami-7 <charlieken221@gmail.com>
2024-10-02 00:12:07 +0530

    removed Code - OSS folder

--- Code - OSS/User/History/6ec26d24/zpgG.js
+++
@@ -1,7 +0,0 @@
-function booleanToString(b) {
-    return b.toString();
-  }
-  
-  // Examples
-  console.log(booleanToString(true));  // Output: "true"
-  console.log(booleanToString(false)); // Output: "false"
