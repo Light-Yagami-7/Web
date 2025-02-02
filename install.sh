5bb96d61cb55d603f2d971a6c25ba212fa04a689
Light-Yagami-7 <charlieken221@gmail.com>
2025-02-02 22:45:19 +0530

    Files have been changed in the last 24 hours

--- install.sh
+++ install.sh
@@ -5,10 +5,13 @@ GITHUB_REPO="https://github.com/Light-Yagami-7/Web.git"
 CONFIG_DIR="$HOME/.config"  # User's config directory
 PACKAGES="waybar"  # Add other packages you want to install here
 
-
 # Clone the GitHub repo containing your new config files from the master branch
 echo "Cloning config files from GitHub (master branch)..."
 git clone --branch master --single-branch "$GITHUB_REPO" "$CONFIG_DIR/your-config-repo"
+
+echo "Installing Zsh"
+sudo pacman -S zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Replace current config files with the ones from the GitHub repo
 echo "Replacing current config files with the ones from GitHub..."
@@ -18,6 +21,13 @@ cp -r "$CONFIG_DIR/your-config-repo"/* "$CONFIG_DIR/"
 echo "Installing packages: $PACKAGES..."
 sudo pacman -Syu --noconfirm $PACKAGES
 
+echo "Setting up atuin..."
+yay -S atuin
+atuin init
+
+echo "Installing yazi (a TUI file manager)"
+sudo pacman -S yazi
+
 # Clean up
 echo "Cleaning up temporary files..."
 rm -rf "$CONFIG_DIR/your-config-repo"
@@ -26,3 +36,5 @@ echo "Done! Your config files have been replaced and the necessary packages inst
 
 echo "Everything is almost done now setting up the lock manger..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh)"
+
+
