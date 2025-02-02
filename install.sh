dbcdfee88bc78477cb5ab5223bbfda90e393c5ee
Light-Yagami-7 <charlieken221@gmail.com>
2025-02-02 20:18:34 +0530

    Files have been changed in the last 24 hours

--- install.sh
+++ install.sh
@@ -5,9 +5,10 @@ GITHUB_REPO="https://github.com/Light-Yagami-7/Web.git"
 CONFIG_DIR="$HOME/.config"  # User's config directory
 PACKAGES="waybar"  # Add other packages you want to install here
 
-# Clone the GitHub repo containing your new config files
-echo "Cloning config files from GitHub..."
-git clone "$GITHUB_REPO" "$CONFIG_DIR/your-config-repo"
+
+# Clone the GitHub repo containing your new config files from the master branch
+echo "Cloning config files from GitHub (master branch)..."
+git clone --branch master --single-branch "$GITHUB_REPO" "$CONFIG_DIR/your-config-repo"
 
 # Replace current config files with the ones from the GitHub repo
 echo "Replacing current config files with the ones from GitHub..."
@@ -23,3 +24,5 @@ rm -rf "$CONFIG_DIR/your-config-repo"
 
 echo "Done! Your config files have been replaced and the necessary packages installed."
 
+echo "Everything is almost done now setting up the lock manger..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh)"
