fb6c5a361c4f635f3eee0e82a9dc779b66f92bd5
Quazarex <140303675+Light-Yagami-7@users.noreply.github.com>
2026-01-20 21:10:14 +0530

    Delete html, css and java directory

--- html, css and java/hello.js
+++
@@ -1,7 +0,0 @@
-function greet() {
-    document.querySelector('form').addEventListener('submit', function(event){
-        let name = document.querySelector('#name').value;
-        alert('hello, ' + name);
-        event.preventDefault();
-    });
-}
